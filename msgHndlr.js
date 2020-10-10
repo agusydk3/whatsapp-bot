25805ad6f50df61cdc0686b887e891464c872812
Agus Purba <57493908+agusydk3@users.noreply.github.com>
2020-10-11 06:47:06 +0700

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -18,7 +18,7 @@ moment.tz.setDefault('Asia/Jakarta').locale('id')
 
 module.exports = msgHandler = async (client, message) => {
     try {
-        const { type, id, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+        const { type, id, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, mentionedJidList } = message
         let { body } = message
         const { name, formattedTitle } = chat
         let { pushname, verifiedName } = sender
